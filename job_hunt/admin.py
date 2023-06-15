ab660766383eceff92f01d153257ebf313cb7c97
davidgaribay0 <dm1garibay@gmail.com>
2023-06-15 19:40:56 -0400

    fix formats

--- job_hunt/admin.py
+++ job_hunt/admin.py
@@ -8,16 +8,18 @@ class AttachmentInline(admin.TabularInline):
     model = Attachment
     extra = 1
 
+
 class EventInline(admin.StackedInline):
     model = Event
     extra = 1
+
 
 @admin.register(JobApplication)
 class JobApplicationAdmin(admin.ModelAdmin):
     list_filter = ['status', 'date_applied']
     inlines = [AttachmentInline, EventInline]
     list_editable = ['status']
-    list_display = ('title', 'company', 'status', 'date_applied','get_events' )
+    list_display = ('title', 'company', 'status', 'date_applied', 'get_events')
 
     def get_events(self, obj):
         return len([event.event_name for event in obj.events.all()])
@@ -29,6 +31,7 @@ class ProfileInline(admin.StackedInline):
     model = Profile
     extra = 1
 
+
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     inlines = [ProfileInline]
@@ -38,6 +41,7 @@ class CompanyAdmin(admin.ModelAdmin):
 @admin.register(Attachment)
 class AttachmentsAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
